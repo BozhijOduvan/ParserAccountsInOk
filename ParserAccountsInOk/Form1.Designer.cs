fedceeacc3de0794af313bc2d4bbda2fee6989fe
BozhijOduvan <garasov2011@yandex.ru>
2022-10-08 21:46:26 +0300

    Первый коммит

--- ParserAccountsInOk/Form1.Designer.cs
+++ ParserAccountsInOk/Form1.Designer.cs
@@ -35,16 +35,12 @@ namespace ParserAccountsInOk
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.timer3 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer4 = new System.Windows.Forms.Timer(this.components);
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -78,14 +74,6 @@ namespace ParserAccountsInOk
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(278, 153);
             this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(45, 217);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(278, 161);
-            this.textBox2.TabIndex = 6;
             // 
             // label2
             // 
@@ -121,14 +109,6 @@ namespace ParserAccountsInOk
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 11;
             // 
-            // progressBar1
-            // 
-            this.progressBar1.Enabled = false;
-            this.progressBar1.Location = new System.Drawing.Point(436, 392);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(187, 23);
-            this.progressBar1.TabIndex = 12;
-            // 
             // timer4
             // 
             this.timer4.Interval = 300;
@@ -142,40 +122,16 @@ namespace ParserAccountsInOk
             this.label6.Size = new System.Drawing.Size(0, 13);
             this.label6.TabIndex = 13;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(518, 313);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(637, 310);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 453);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -192,16 +148,12 @@ namespace ParserAccountsInOk
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Timer timer3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer4;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- ParserAccountsInOk/Form1.cs
+++ ParserAccountsInOk/Form1.cs
@@ -6,18 +6,17 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 using System.Text.RegularExpressions;
-using System.Drawing.Printing;
 
 namespace ParserAccountsInOk
 {
     public partial class Form1 : Form
     {
         private static IWebDriver _chromeDriver;
-        private static String _url;
+        private static string _url;
         int x;
-        int counter = 0;
+        
         string page_source;
-        private const bool v = true;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +24,7 @@ namespace ParserAccountsInOk
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            _url = "https://ok.ru/olegmaigov/members"; //"https://ok.ru/olegmaigov/members";
+            _url = "https://ok.ru/olegmaigov/members";
             _chromeDriver = new ChromeDriver();
             _chromeDriver.Navigate().GoToUrl(_url);
             timer3.Enabled = true;
@@ -50,67 +49,51 @@ namespace ParserAccountsInOk
             {
                 timer3.Enabled = false;
                 timer1.Enabled = false;
-                timer4.Enabled = true;
+                
                 x = 0;
                 label1.Text = x.ToString();
                 timer2.Enabled = true;
                 PoiskLink();
             }
         }
-        private void ProgressChanged()
-        {
-            counter++;
-            switch (counter % 6)
-            {
-                case 0: label6.Text = "Идёт поиск."; break;
-                case 1: label6.Text = "Идёт поиск.."; break;
-                case 2: label6.Text = "Идёт поиск..."; break;
-                case 3: label6.Text = "Идёт поиск...."; break;
-                case 4: label6.Text = "Идёт поиск....."; break;
-                case 5: label6.Text = "Идёт поиск......"; break;
-            }
-        }
+
+       
+
         void PoiskLink()
         {
             new Thread(() =>
             {
                 Thread.CurrentThread.IsBackground = true;
-                IList <IWebElement> links = _chromeDriver.FindElements(By.TagName("a"));
-            foreach (var link in links)
-            {
-                if (link.Text == "Показать ещё")
+                IList<IWebElement> links = _chromeDriver.FindElements(By.TagName("a"));
+                foreach (var link in links)
                 {
-                    link.Click();
-                    new Thread(() => { BeginInvoke(new Action(() => timer3.Enabled = true)); timer4.Enabled = false; }).Start();
+                    if (link.Text == "Показать ещё")
+                    {
+                        link.Click();
+                        new Thread(() => { BeginInvoke(new Action(() => timer3.Enabled = true)); }).Start();
+                        return;
+                    }
+                }
+                {
+                    new Thread(() => { BeginInvoke(new Action(() => copyHTML())); }).Start();
+                    new Thread(() => { BeginInvoke(new Action(() => timer2.Enabled = false)); }).Start();
                     return;
                 }
-            }
-            {
-                new Thread(() => { BeginInvoke(new Action(() => copyHTML())); }).Start();
-                new Thread(() => { BeginInvoke(new Action(() => timer2.Enabled = false)); }).Start();
-                return;
-            }
-        }).Start();
-    }
-
-        
-
-
+            }).Start();
+        }
 
         void parsUrl()
         {
             //string link =textBox1.Text;
-            
-                MatchCollection match = Regex.Matches(page_source, @"\""/profile(.*?)\?st\.(.*?)""");//@"\""/profile(.*?)"""
 
+            MatchCollection match = Regex.Matches(page_source, @"\""/profile(.*?)\?st\.(.*?)""");//@"\""/profile(.*?)"""
             foreach (Match m in match)
             {
-                textBox2.Text += "https://ok.ru/profile" + m.Groups[1].Value + Environment.NewLine;
+                textBox1.Text += "https://ok.ru/profile" + m.Groups[1].Value + Environment.NewLine;
             }
-
-            textBox2.Lines = textBox2.Lines.Distinct().ToArray(); //удаляем дубли
+            textBox1.Lines = textBox1.Lines.Distinct().ToArray(); //удаляем дубли
             label5.Text = DateTime.Now.ToString();
-           
+
         }
 
         private void timer2_Tick(object sender, EventArgs e)
@@ -118,7 +101,7 @@ namespace ParserAccountsInOk
             if (x == 4)
             {
                 timer3.Enabled = false;
-                timer4.Enabled = true;
+                
                 PoiskLink();
                 x = 0;
                 label1.Text = x.ToString();
@@ -129,36 +112,23 @@ namespace ParserAccountsInOk
         {
             //Скроллбар в низ
             downScroll();
-
         }
 
         void copyHTML()
         {
             timer2.Enabled = false;
-            
-                page_source = _chromeDriver.PageSource;
+
+            page_source = _chromeDriver.PageSource;
             //textBox1.Text = page_source;
-           
-             Thread.Sleep(1000);
-         
 
-            parsUrl();
-        }
+            Thread.Sleep(1000);
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-             page_source = _chromeDriver.PageSource;
-             textBox1.Text = page_source;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
             parsUrl();
         }
 
         private void timer4_Tick(object sender, EventArgs e)
         {
-            ProgressChanged();
+            
         }
     }
 }
